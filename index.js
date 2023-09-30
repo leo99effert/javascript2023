82bd0f5373efd658b92bce9d5cbf4233a1753a5b
leo99effert <leo99effert@outlook.com>
2023-09-30 15:20:15 +0200

    -

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('g');
+/**
+ * check for errors first
+ */
+
+let person = 'sune';
+function printPerson(person) {
+  if ( 
+    typeof person === 'undefined' ||
+    person.trim() === '' || 
+    person === 'none'
+  ) {
+    console.log('person is not valid');
+    return;
+  }
+
+  console.log('the person is: ' + person);
+}
+printPerson(person);
